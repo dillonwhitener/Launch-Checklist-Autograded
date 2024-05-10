3b04bbcdadee6d408d13d05c377c6d06abfc5e0b
dillonwhitener <djwhitener@gmail.com>
2024-05-10 11:15:48 -0500

    Revised Curly Bracket location

--- scriptHelper.js
+++ scriptHelper.js
@@ -3,7 +3,7 @@ require('cross-fetch/polyfill');
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
     let target = document.getElementById('missionTarget');
     target.innerHTML =
-                 `<h2>Mission Destination</h2>
+        `<h2>Mission Destination</h2>
                  <ol>
                      <li>Name: ${name} </li>
                      <li>Diameter: ${diameter} </li>
@@ -42,38 +42,38 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     }
     else if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number") {
         alert("Please enter valid names for pilot or co-pilot that do NOT include numbers");
-    }
-    else {
+    } else {
         pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
         copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
 
-    } if (Number(fuelLevel) < 10000 && Number(cargoLevel) <= 10000) {
-        list.style.visibility = "visible";
-        fuelStatus.innerHTML = "Fuel level too low for launch";
-        cargoStatus.innerHTML = "Cargo mass low enough for launch";
-        launchStatus.style.color = "red";
-        launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-    }
-    else if (Number(cargoLevel) > 10000 && Number(fuelLevel) >= 10000) {
-        list.style.visibility = "visible";
-        fuelStatus.innerHTML = "Fuel level high enough for launch";
-        cargoStatus.innerHTML = "Cargo mass too heavy for launch";
-        launchStatus.style.color = "red";
-        launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-    }
-    else if (Number(cargoLevel) > 10000 && Number(fuelLevel) < 10000) {
-        list.style.visibility = "visible";
-        fuelStatus.innerHTML = "Fuel level too low for launch";
-        cargoStatus.innerHTML = "Cargo mass too heavy for launch";
-        launchStatus.style.color = "red";
-        launchStatus.innerHTML = "Shuttle Not Ready for Launch";
-    }
-    else {
-        list.style.visibility = "visible";
-        fuelStatus.innerHTML = "Fuel level high enough for launch";
-        cargoStatus.innerHTML = "Cargo mass low enough for launch";
-        launchStatus.style.color = "green";
-        launchStatus.innerHTML = "Shuttle is Ready for Launch";
+        if (Number(fuelLevel) < 10000 && Number(cargoLevel) <= 10000) {
+            list.style.visibility = "visible";
+            fuelStatus.innerHTML = "Fuel level too low for launch";
+            cargoStatus.innerHTML = "Cargo mass low enough for launch";
+            launchStatus.style.color = "red";
+            launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+        }
+        else if (Number(cargoLevel) > 10000 && Number(fuelLevel) >= 10000) {
+            list.style.visibility = "visible";
+            fuelStatus.innerHTML = "Fuel level high enough for launch";
+            cargoStatus.innerHTML = "Cargo mass too heavy for launch";
+            launchStatus.style.color = "red";
+            launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+        }
+        else if (Number(cargoLevel) > 10000 && Number(fuelLevel) < 10000) {
+            list.style.visibility = "visible";
+            fuelStatus.innerHTML = "Fuel level too low for launch";
+            cargoStatus.innerHTML = "Cargo mass too heavy for launch";
+            launchStatus.style.color = "red";
+            launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+        }
+        else {
+            list.style.visibility = "visible";
+            fuelStatus.innerHTML = "Fuel level high enough for launch";
+            cargoStatus.innerHTML = "Cargo mass low enough for launch";
+            launchStatus.style.color = "green";
+            launchStatus.innerHTML = "Shuttle is Ready for Launch";
+        }
     }
 }
 
@@ -88,7 +88,7 @@ async function myFetch() {
 }
 
 function pickPlanet(planets) {
-   let index = Math.random() * planets.length
+    let index = Math.random() * planets.length
     return planets[Math.floor(index)];
 }
 
